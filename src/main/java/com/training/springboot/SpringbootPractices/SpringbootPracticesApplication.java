90acd945d87a4ce3f8129904283de8c8c592cf73
JoJo <jojo141a@hotmail.com>
2017-09-10 11:08:45 +0700

    init project

---
+++ src/main/java/com/training/springboot/SpringbootPractices/SpringbootPracticesApplication.java
@@ -0,0 +1,12 @@
+package com.training.springboot.SpringbootPractices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPracticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPracticesApplication.class, args);
+	}
+}
