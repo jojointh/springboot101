c69816910732231133dc452e4b3625c3110a1d9c
JoJo <jojo141a@hotmail.com>
2017-09-10 16:55:04 +0700

    change getall, use spring data repository

--- src/main/java/com/training/springboot/SpringbootPractices/repository/EmployeeCrudRepository.java
+++ src/main/java/com/training/springboot/SpringbootPractices/repository/EmployeeCrudRepository.java
@@ -3,6 +3,12 @@ package com.training.springboot.SpringbootPractices.repository;
 import com.training.springboot.SpringbootPractices.domain.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeCrudRepository extends JpaRepository<Employee, Integer> {
+
+    List<Employee> findByFirstName(String firstName);
+    // findBy...
+    Employee findByLastName(String lastName);
 
 }
