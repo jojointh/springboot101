e9374f77f986a2a9f341f00a7f0de28a50c9e375
JoJo <jojo141a@hotmail.com>
2017-09-10 11:36:14 +0700

    update GET employee to return json

--- src/main/java/com/training/springboot/SpringbootPractices/controller/EmployeeController.java
+++ src/main/java/com/training/springboot/SpringbootPractices/controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.training.springboot.SpringbootPractices.controller;
 
+import com.training.springboot.SpringbootPractices.domain.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,11 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
 
     @RequestMapping(value = "/employees", method = RequestMethod.GET)
-    public String listAllAnnotation() {
+    public Employee listAllAnnotation() {
 
         System.out.println("### Enter to GET: /employees ###");
 
-        return "hello 2";
+        Employee employee = new Employee();
+        employee.setId(1);
+        employee.setFirstName("Surasak");
+        employee.setLastName("Ad.");
+        employee.setAge(20);
 
+        return employee;
     }
 }
